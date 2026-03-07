9411e381cb9741a7b386ffd7a7071682b64649f3
elena.vanengelen <elena.vanengelen@edsn.nl>
2026-03-07 20:33:21 +0100

    fix: downgrade shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("plugin.serialization") version "2.3.0" apply false
     id("org.springframework.boot") version "4.0.2" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
-    id("com.gradleup.shadow") version "9.3.2" apply false
+    id("com.gradleup.shadow") version "8.3.6" apply false
     id("org.jetbrains.kotlinx.kover") version "0.9.4"
 }
 
