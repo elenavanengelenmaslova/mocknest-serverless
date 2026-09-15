0785f97d5e32edbfcd0cbf92a101bda15cb182be
elena.vanengelen <elena.vanengelen@edsn.nl>
2026-09-15 05:57:36 +0200

    fix: downgradle kotlin so codeql supports it

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.4.20" apply false
-    kotlin("plugin.serialization") version "2.4.20" apply false
+    kotlin("jvm") version "2.4.10" apply false
+    kotlin("plugin.serialization") version "2.4.10" apply false
     id("com.gradleup.shadow") version "9.6.1" apply false
     id("org.jetbrains.kotlinx.kover") version "0.9.9"
 }
